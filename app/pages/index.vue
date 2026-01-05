c3e26ad9b369047fc99178bc5c5063388f0f6cd4
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-01-05 11:13:22 +0700

    Setup Tailwind and Daisy UI

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="mockup-browser border border-base-300 m-10 pb-30">
+        <div class="mockup-browser-toolbar">
+            <div class="input">https://daisyui.com</div>
+        </div>
+        <div class="grid place-content-center h-80">Hello!</div>
+    </div>
+</template>
